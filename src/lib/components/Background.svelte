f0a5a8464677fe8937cff8d9d3b76b5ff14bdb7d
Amaan-Dhanani <120526048+Amaan-Dhanani@users.noreply.github.com>
2025-05-13 17:17:28 +0000

    this is a lot

---
+++ src/lib/components/Background.svelte
@@ -0,0 +1,19 @@
+<script>
+	import Logo from '$lib/images/Logo.png';
+</script>
+
+<div class="relative">
+	<p class="invisible">rrr</p>
+	<div class="rounded-t-[70px] bg-[#F8FAFE] ml-[50px] mr-[50px] pt-[45px] px-auto mt-[80px]">
+		<img src={Logo} alt="" class="max-w-[100px] max-w-[80px] rounded-[20px]" />
+		<div class="flex justify-between items-center">
+			<div class="flex-1 font-balsamiq-sans text-[80px] font-bold leading-normal p-0 m-0">
+				<div><slot name="heading"></slot></div>
+				<div class="text-[#000] font-poppins text-[16px] font-normal leading-[31px]"><slot name="paragraph"></slot></div>
+			</div>
+			<div class="flex-1">
+                <div><slot name="image"></slot></div>
+            </div>
+		  </div>
+	</div>
+</div>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,16 @@
+<script>
+	import Background from "@lib/components/Background.svelte";
+	//PUT CHARACTER NAME
+	//       ↓                       ↓
+	import Piaru from "$lib/images/Piaru.png"
+</script>
+
+<Background>
+	<!--      ADJUST        ↓         ↓         ↓           ↓-->
+	<img slot="image" width="500" height=""  alt="Piaru" src={Piaru}>
+
+	<span class="text-[#BCBC79]" slot="heading">PIARU</span>
+
+	<!--      ADJUST        ↓                      ↓           -->
+	<span class="text-[#383838]" slot="paragraph">Piaru is a character whose name is a fusion of “Piano” and “Haru,” the latter meaning spring or rebirth—symbolizing both musical essence and renewal. Piaru’s abilities include Resonant Beam, which emits emotional soundwaves powerful enough to cleanse negativity, and Key Storm, a cyclone of piano keys used for both offense and defense. However, Piaru’s greatest weakness is silence—without sound, their power fades, leaving them vulnerable.</span>
+</Background>
