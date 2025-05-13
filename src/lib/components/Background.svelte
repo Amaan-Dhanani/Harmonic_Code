3fba8be80b4f8fef2873a8f1dbf53fe6badf4e6f
Amaan-Dhanani <120526048+Amaan-Dhanani@users.noreply.github.com>
2025-05-13 17:39:21 +0000

    added border, will  fix

--- src/lib/components/Background.svelte
+++ src/lib/components/Background.svelte
@@ -4,8 +4,8 @@
 
 <div class="relative">
 	<p class="invisible">rrr</p>
-	<div class="rounded-t-[70px] bg-[#F8FAFE] ml-[50px] mr-[50px] pt-[45px] px-auto mt-[80px]">
-		<img src={Logo} alt="" class="max-w-[100px] max-w-[80px] rounded-[20px]" />
+	<div class="rounded-t-[70px] bg-[#F8FAFE] ml-[50px] mr-[50px] pt-[45px] align-center mt-[80px]">
+		<img src={Logo} alt="" class="max-w-[150px] max-w-[80px] rounded-[20px] ml-[40px] border" />
 		<div class="flex justify-between items-center">
 			<div class="flex-1 font-balsamiq-sans text-[80px] font-bold leading-normal p-0 m-0">
 				<div><slot name="heading"></slot></div>
