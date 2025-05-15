8a24b7500959d9e3ab509381bee14b91eab0c75f
Misuking <misucddr@gmail.com>
2025-05-15 15:48:40 +0000

    whatever

--- src/routes/characters/violia/+page.svelte
+++ src/routes/characters/violia/+page.svelte
@@ -14,10 +14,10 @@
 
 	<span class="text-[#383838]" slot="paragraph">
 		<li>Derived from the fusion of Violia, a string instrument symbolizing discord, and Euphoria/Anhedonia, representing the contrast between intense pleasure and a lack of it, embodying a twisted emotional state.</li>
-		<li>Violia can paralyze her victims by emitting piercing, discordant screeches from her violin. The chaotic sound disrupts the listener's mental state, leaving them vulnerable and immobilized.</li>
-		<li>Piaru’s music is her greatest vulnerability. The positive, harmonious frequencies he plays can weaken Violia’s will, disrupting her chaotic powers. When exposed to Piaru’s melodies, her emotional discord is soothed, rendering her more susceptible to defeat.</li>
-		<li>Violia carries an air of refinement and grace, often displaying an aura of aristocratic elegance. Her sophistication enhances her ability to manipulate others through her artistry, whether it be through subtle gestures or the compelling allure of her music.</li>
+		<li>Violia can paralyze her victims by emitting piercing, discordant screeches from her violin.</li>
+		<li>Piaru’s music is her greatest vulnerability. The positive, harmonious frequencies he plays can weaken Violia’s will, disrupting her chaotic powers.</li>
+		<li>Violia carries an air of refinement and grace, often displaying an aura of aristocratic elegance.</li>
 		<li>Deeply resentful and cynical, Violia’s bitterness stems from a life filled with emotional conflict. Her sharp, discordant music reflects her inner turmoil and rejection of peace or resolution.</li>
-		<li>Violia’s pain and anguish fuel her art. Her violin is not just an instrument; it is a reflection of her tortured soul. She is constantly battling with her inner demons, and her music serves as both an outlet and a weapon, channeling her torment into every note she plays.</li>
+		<li>Violia’s pain and anguish fuel her art. She is constantly battling with her inner demons, and her music serves as both an outlet and a weapon, channeling her torment into every note she plays.</li>
 	</span>
 </Background>
