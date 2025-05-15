e3fbf64345f9a15b1f5af1099aaeabdc8cebc296
Misuking <misucddr@gmail.com>
2025-05-15 15:51:45 +0000

    Piaru

--- src/routes/characters/piaru/+page.svelte
+++ src/routes/characters/piaru/+page.svelte
@@ -14,11 +14,11 @@
 	<!--      ADJUST        ↓                      ↓           -->
 	<span class="text-[#383838]" slot="paragraph">
 		<li>A fusion of Haru (Japanese for "Spring") and Piano, symbolizing both nature and music.</li>
-		<li>Piaru's music produces harmonious frequencies that counteract the malevolent waves emitted by enemies, particularly Viola, whose violin generates evil frequencies.</li>
+		<li>Piaru's music produces harmonious frequencies that counteract the malevolent waves emitted by enemies, particularly Viola.</li>
 		<li>Raised in the plains of Pianotopia, where he cultivated a deep connection with nature and the land.</li>
 		<li>Piaru sustains himself with fresh vegetables and organically raised meat, emphasizing a natural and wholesome lifestyle.</li>
 		<li>A child of the plains, Piaru has an inherent understanding and talent for the wild, reflecting his symbiotic relationship with the environment.</li>
 		<li>Piano serves as his primary weapon, with his positive, harmonious compositions acting as a direct force against evil frequencies.</li>
-		<li>Piaru's identity is rooted in the fusion of nature and music—his piano harmonies represent his soul's connection to the natural world, and together, they form his power to resist darkness.</li>
+		<li>Piaru's identity is rooted in the fusion of nature and music—his piano harmonies represent his soul's connection to the natural world.</li>
 	</span>
 </Background>
